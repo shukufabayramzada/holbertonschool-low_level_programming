17c14348598818a09b6c48f6e42869a0cb0c30eb
shukufabayramzada <shukufabayramzada@gmail.com>
2024-02-19 12:16:40 +0000

    content was updated

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,12 +1,10 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main -function
  * Return: Always 1 (success)
  **/
 int main(void)
 {
-           write(2,
-			   	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-				      	      59);
-           return (1);
+	write(2,	"and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	return (1);
 }
