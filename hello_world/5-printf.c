443a113da5ad35af1a77efa68e0021072c57e079
shukufabayramzada <shukufabayramzada@gmail.com>
2024-02-19 10:57:32 +0000

    file was created

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - function
+ *
+ * Return: Always 0 (success)
+ *
+ **/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
