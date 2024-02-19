dab71802a999b50196f32d69e8e39e2f38c70568
shukufabayramzada <shukufabayramzada@gmail.com>
2024-02-19 10:30:39 +0000

    content was updated

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,4 @@
 #include <stdio.h>
-
-/**
- * main - Prints "Programming is like building a multilingual
- *                   puzzle, followed by a new line.
- *   
- * Return: Always 0.
- *   
- */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
