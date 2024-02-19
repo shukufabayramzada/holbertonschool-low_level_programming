e11aaed4ab5afc78586ab99cda404af1094cb714
shukufabayramzada <shukufabayramzada@gmail.com>
2024-02-19 11:56:19 +0000

    file was created

---
+++ hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -function
+ * Return: Always 1 (success)
+ **/
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\"- Dora Korpar, 2015-10-19");
+	return (1);
+}
