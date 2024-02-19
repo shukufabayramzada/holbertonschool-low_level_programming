8bf96bf26634f70da08715b1e70c1b2868a77a09
shukufabayramzada <shukufabayramzada@gmail.com>
2024-02-19 12:05:33 +0000

    content was updated

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -5,6 +5,8 @@
  **/
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\"- Dora Korpar, 2015-10-19");
-	return (1);
+           write(2,
+			   	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+				      	      59);
+           return (1);
 }
