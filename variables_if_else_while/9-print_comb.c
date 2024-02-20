7e299f010cf62798b3993b4040158629765cea68
shukufabayramzada <shukufabayramzada@gmail.com>
2024-02-20 18:45:29 +0000

    file has been created

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - function or entry point
+ * Return: Always 0 (success)
+ **/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num + '0');
+		if (num == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
