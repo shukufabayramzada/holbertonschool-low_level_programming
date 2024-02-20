762340c1728cd6748a01383c7656f7737b3a858c
shukufabayramzada <shukufabayramzada@gmail.com>
2024-02-20 17:15:23 +0000

    file was created

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 /**
- * main -function or entry point
+ * main - function or entry point
  * Return: Always 0 (success)
  **/
 int main(void)
 {
-	int letter;
+	char low, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		if (low != e && low != q)
+		{
+			putchar(low);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
