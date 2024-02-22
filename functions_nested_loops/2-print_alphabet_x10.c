922233ec345e0e70de17db86314120779a0d8c67
shukufabayramzada <shukufabayramzada@gmail.com>
2024-02-22 09:41:35 +0000

    file was created

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function or entry point
+ * Return: Always 0 (success)
+ **/
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		c = 'a';
+		while ( c <= 'z')
+		{
+			_putchar(c);
+				c++;
+		}
+		_putchar('\n');
+	}
+}
+
+
