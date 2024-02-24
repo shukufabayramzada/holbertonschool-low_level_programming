ce73c9c35fc73c99a026705744259ea673d011aa
shukufabayramzada <shukufabayramzada@gmail.com>
2024-02-24 11:50:21 +0000

    file has succesfully been created

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - function or entry point
+ * @n: number that will be printed
+ * Return: always 0
+ **/
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d ", n);
+			}
+		}
+	} else
+	{
+		for (; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d ", n);
+			}
+		}
+	}
+}
