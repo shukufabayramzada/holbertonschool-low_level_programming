c9ccbb171ce7a9a5673a94fc0e24c3c345aacfd9
shukufabayramzada <shukufabayramzada@gmail.com>
2024-02-26 16:20:45 +0000

    time-table with specific structure

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main -function or entry point
+ * Return: Always 0 success.
+ **/
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 99; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", i);
+			if (i != 100)
+				printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
