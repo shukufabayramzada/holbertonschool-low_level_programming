948becf32fb8782cf2d2ebdce67e8445da2d18ca
shukufabayramzada <shukufabayramzada@gmail.com>
2024-02-26 12:58:20 +0000

    content was updated

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_square - function or entry point
+ * @size: size of the square
+ **/
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
+
