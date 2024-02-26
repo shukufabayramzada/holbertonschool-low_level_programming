15fab6c7761d6174ad72e5195241c6c5f531380c
shukufabayramzada <shukufabayramzada@gmail.com>
2024-02-26 11:00:41 +0000

    content was updated

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -12,10 +12,9 @@ void more_numbers(void)
 		for (i = 0; i < 15; i++)
 		{
 			if (i > 9)
-			{
-				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
-			}
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			
 		}
 		_putchar('\n');
 	}
