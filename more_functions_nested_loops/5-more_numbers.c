3d40269e52b329242f97afc69986e06a139aa9e2
shukufabayramzada <shukufabayramzada@gmail.com>
2024-02-26 10:57:17 +0000

    file was created

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - function or entry point
+ * Return: expected result for function
+ **/
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			if (i > 9)
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
