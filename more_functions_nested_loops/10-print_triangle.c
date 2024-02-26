8a434c5a3dd1b38de72b033fd91a5b5acb7fe6ec
shukufabayramzada <shukufabayramzada@gmail.com>
2024-02-26 17:33:49 +0000

    file was created

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_triangle - function or entry point
+ * @size: character that will be checked
+ **/
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i < size; i++)
+		{
+			for (j = size; j >= 1; j--)
+			{
+				if (i < j)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
+
