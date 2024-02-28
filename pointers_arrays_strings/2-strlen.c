551523c29ae1abea26c97f030d85b20e615b0e6d
shukufabayramzada <shukufabayramzada@gmail.com>
2024-02-28 17:10:31 +0000

    reteurn the length of the string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - return the length of a string
+ * @s: parameter
+ **/
+int _strlen(char *s)
+{
+	int length;
+
+	for (length = 0; s[length] != '\0'; length++)
+	{
+	;
+	}
+	return (length);
+
+}
