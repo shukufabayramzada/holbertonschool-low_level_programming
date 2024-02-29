ed342c1454388a6b30d718515f522224598fca01
shukufabayramzada <shukufabayramzada@gmail.com>
2024-02-29 11:48:57 +0000

    second half of the string

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -5,13 +5,18 @@
 **/
 void puts_half(char *str)
 {
-	int index, start_index, i;
+	int index, start_index, odd_case, i;
 
 	for (index = 0; str[index] != 0; index++)
 	{
 	;
 	}
 	start_index = index / 2;
+	if (index % 2 == 1)
+	{
+	odd_case = 1;
+	start_index = index / 2 + odd_case;
+	}
 	for (i = start_index; i < index; i++)
 	{
 	_putchar(str[i]);
