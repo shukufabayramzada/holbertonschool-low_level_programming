cfec1c599b82ec1987b748a070badff736801cb4
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-01 10:18:20 +0000

    concatenate two strings until n

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *_strncat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ * @n: represent at most bytes
+ * Return: dest
+ **/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		;
+	}
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		if (i != n)
+		{
+			i++;
+		}
+	}
+	return (dest);
+}
