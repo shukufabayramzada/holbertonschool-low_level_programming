284d5248f66c6fe687c8b192357c0e4067a589d4
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-01 17:33:58 +0000

    capitalize all words of string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -6,27 +6,27 @@
  */
 char *cap_string(char *str)
 {
-    int i = 0;
-    int capitalize_next = 1;
+	int conversion, index, count;
 
-	while (str[i] != '\0')
+	char chars[] = {' ', ',', ';', '.', '!',
+			 '?', '"', '(', ')', '{', '}',  '\t', '\n', '\0'};
+	conversion = 32;
+
+	for (index = 0; str[index] != '\0'; index++)
 	{
-        if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-            str[i] == ',' || str[i] == ';' || str[i] == '.' ||
-            str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-            str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
-        {
-            capitalize_next = 1;
-        }
-        else if (capitalize_next && (str[i] >= 'a' && str[i] <= 'z'))
-        {
-	str[i] -= ('a' - 'A');
-	capitalize_next = 0;
+		if (str[index] >= 'a' && str[index] <= 'z')
+		{
+			str[index] =  str[index] - conversion;
+		}
+		conversion = 0;
+		for (count = 0; chars[count] != '\0'; count++)
+		{
+			if (chars[count] == str[index])
+			{
+				conversion = 32;
+				break;
+			}
+		}
 	}
-
-        i++;
-    }
-
-    return (str);
+	return (str);
 }
-
