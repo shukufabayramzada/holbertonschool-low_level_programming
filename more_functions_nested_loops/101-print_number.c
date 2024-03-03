f0f949c20c2f224312cfd178f5f5016d12091cdb
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-03 14:08:14 +0000

    print an integer

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -7,16 +7,14 @@ void print_number(int n)
 {
 	unsigned int num = n;
 
-
 	if (n < 0)
 	{
-	_putchar('-');
-	num = -num;
+		_putchar('-');
+		num = -num;
 	}
 
 	if ((num / 10) > 0)
+		print_number(num / 10);
 
-	print_number(n / 10);
-
-	_putchar((n % 10) + '0');
+	_putchar((num % 10) + '0');
 }
