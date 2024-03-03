ae476298f20d45c20be084c869fff42a95118b96
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-03 15:03:05 +0000

    random password generator

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -2,29 +2,30 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LENGTH 11
+/**
+ * main - Generate a random password for 101-crackme file
+ *
+ * Return: 0 (Success)
+ */
 
-int main(void) {
-    char password[PASSWORD_LENGTH + 1];
-    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    int i;
+int main(void)
+{
+	int password[64], i, sum = 0, n;
 
-    srand(time(NULL));
+	srand(time(NULL));
+	for (i = 0; i < 64; i++)
+	{
+		password[i] = rand() % 78;
+		sum+= password[i] + '0';
+		putchar(password[i] + '0');
 
-	password[0] = charset[rand() % 26 + 26];
-
-	for (i = 1; i < 6; ++i) {
-        password[i] = charset[rand() % 26];
-    }
-
-	for (i = 6; i < 11; ++i) {
-        password[i] = charset[rand() % 10 + 52];
-    }
-
-	password[PASSWORD_LENGTH] = '\0';
-
-    printf("%s\n", password);
-
-    return (0);
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+	return (0);
 }
-
