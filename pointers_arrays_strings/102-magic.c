eec39ebf21a9302fa183763c38bc839ebfb9ec7f
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-03 17:29:31 +0000

    working on an array

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -10,10 +10,16 @@ int main(void)
 	int *p;
 
 	a[2] = 1024;
+	/*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
 	p = &n;
-
 	*(p - 2) = 98;
 	printf("a[2] = %d\n", a[2]);
-
 	return (0);
 }
