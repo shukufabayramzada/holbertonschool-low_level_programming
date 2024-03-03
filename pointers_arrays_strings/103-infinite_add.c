cd7bbd3ecd35163a8c25f35feaa57f4060b91b2d
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-03 18:16:43 +0000

    add two integers

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -1,43 +1,47 @@
 #include <stdio.h>
 #include <string.h>
+/**
+* infinite_add - function adds 2 integers
+*@n1: first number
+*@n2: second number
+* @r: is the buffer that the function will use to store the result
+* @size_r: is the buffer size
+* Return: a pointer to the result
+**/
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int len1 = strlen(n1);
+	int len2 = strlen(n2);
+	int i = len1 - 1;
+	int j = len2 - 1;
+	int k = size_r - 1;
+	int carry = 0;
 
-char* infinite_add(char *n1, char *n2, char *r, int size_r) {
-    int len1 = strlen(n1);
-    int len2 = strlen(n2);
+	while (i >= 0 || j >= 0 || carry > 0)
+	{
+	int sum = carry;
 
-    
-    int i = len1 - 1;
-    int j = len2 - 1;
-    int k = size_r - 1;
+	if (i >= 0)
+	{
+	sum += n1[i] - '0';
+	i--;
+	}
 
-    
-    int carry = 0;
+	if (j >= 0)
+	{
+	sum += n2[j] - '0';
+	j--;
+	}
 
-    while (i >= 0 || j >= 0 || carry > 0) {
-        int sum = carry;
+	if (k < 0)
+	{
+	return (0);
+	}
 
-        if (i >= 0) {
-            sum += n1[i] - '0';
-            i--;
-        }
+	r[k] = (sum % 10) + '0';
+	carry = sum / 10;
 
-        if (j >= 0) {
-            sum += n2[j] - '0';
-            j--;
-        }
-
-        if (k < 0) {
-            return 0;
-        }
-
-       
-        r[k] = (sum % 10) + '0';
-        carry = sum / 10;
-
-       
-        k--;
-    }
-
-    
-    return &r[k + 1];
+	k--;
+}
+	return (&r[k + 1]);
 }
