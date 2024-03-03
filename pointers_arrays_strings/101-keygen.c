744ff66f8ede77a3da703e92e759312f0b1a6d02
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-03 14:46:55 +0000

    random password generator

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -5,20 +5,20 @@
 #define PASSWORD_LENGTH 11
 
 int main(void) {
-    char password[PASSWORD_LENGTH + 1]; // +1 for null terminator
+    char password[PASSWORD_LENGTH + 1];
     const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     int i;
 
-    srand(time(NULL)); // Seed random number generator with current time
+    srand(time(NULL));
 
     for (i = 0; i < PASSWORD_LENGTH; ++i) {
-        password[i] = charset[rand() % (sizeof(charset) - 1)]; // Pick random character from charset
+        password[i] = charset[rand() % (sizeof(charset) - 1)];
     }
 
-    password[PASSWORD_LENGTH] = '\0'; // Null-terminate the string
+    password[PASSWORD_LENGTH] = '\0';
 
-    printf("%s\n", password); // Print the generated password
+    printf("%s\n", password);
 
-    return 0;
+    return (0);
 }
 
