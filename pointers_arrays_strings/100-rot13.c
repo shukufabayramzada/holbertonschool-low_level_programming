f09527641d3ea47e0cc062b620e48a1be94cfe89
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-03 17:07:10 +0000

    betty coding style was added

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -1,30 +1,27 @@
 #include "main.h"
-
 /**
  * rot13 - Encodes a string using ROT13 cipher
  * @s: The string to be encoded
- *
  * Return: Pointer to the encoded string
- */
+ **/
 char *rot13(char *s)
 {
-    char *ptr = s;
-    char *alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    char *rot13 = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-    int i;
+	char *ptr = s;
+	char *alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *rot13 = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int i;
 
-    for (; *ptr != '\0'; ptr++)
-    {
-        for (i = 0; alphabet[i] != '\0'; i++)
-        {
-            if (*ptr == alphabet[i])
-            {
-                *ptr = rot13[i];
-                break;
-            }
-        }
-    }
-
-    return (s);
+	for (; *ptr != '\0'; ptr++)
+	{
+	for (i = 0; alphabet[i] != '\0'; i++)
+	{
+	if (*ptr == alphabet[i])
+	{
+	*ptr = rot13[i];
+	break;
+	}
+	}
+	}
+	return (s);
 }
 
