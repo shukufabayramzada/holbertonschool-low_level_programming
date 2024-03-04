bc5f5de4e5d3b2925811b439b24f533b15c86008
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-04 13:31:32 +0000

    locates a substring

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -14,13 +14,13 @@ char *_strstr(char *haystack, char *needle)
 	{
 	for (j = 0; needle[j] != '\0'; j++)
 	{
-	if (haystack[i] == needle[j])
+	if (haystack[i + j] != needle[j])
 	{
-	return (&haystack[i]);
+	break;
 	}
 	if (needle[j] == '\0')
 	{
-	break;
+	return (&haystack[i]);
 	}
 	}
 	}
