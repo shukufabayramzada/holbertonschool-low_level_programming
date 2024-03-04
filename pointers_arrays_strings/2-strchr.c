925bf5dfa4d8a8e56eb270ea1bedeba0bcb09275
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-04 11:21:31 +0000

    locates the characters in the string

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -10,11 +10,15 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; (s[i] != c) && (s[i] != '\0'; i++)
-			;
+	for (i = 0; (s[i] != c) && (s[i] != '\0'); i++)
+	{
+	;
+	}
 
-			if (s[i] == c)
-			return (s + i);
-			else
-			return (NULL);
+	if (s[i] == c)
+	{
+	return (s + i);
+	}
+	else
+	return (NULL);
 }
