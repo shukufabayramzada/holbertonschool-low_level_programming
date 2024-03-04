118255b8a2ea49bfb5790e8b0a9c2c4f3d447237
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-04 12:31:43 +0000

    gets the length of a prefix substring

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -11,18 +11,12 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-	;
-	}
 	for (j = 0; accept[j] != '\0'; j++)
 	{
-	;
-	}
-	while (s[i] != accept[j])
-	{
-	i++;
 	if (s[i] == accept[j])
 	{
-	return (s[i]);
+	break;
+	}
 	}
 	}
 	return (0);
