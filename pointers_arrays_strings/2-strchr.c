2c3806f034f3a3a0b98ea57ce52cfd7764727f70
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-04 11:08:41 +0000

    locates a character in a string

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -6,12 +6,14 @@
  * @s: string
  * Return: a pointer to the first occurence of the character c
  **/
-char *_strchr(char *s, char c) {
-	  while (*s != '\0') {
-		    if (*s == c) {
-			      return s;
-			        }
-		      s++;
-		        }
-	    return NULL;
+char *_strchr(char *s, char c)
+{
+		int i;
+
+			for (i = 0; (s[i] != c) && (s[i] != '\0'); i++)
+						;
+				if (s[i] == c)
+							return (s + i);
+					else
+								return (NULL);
 }
