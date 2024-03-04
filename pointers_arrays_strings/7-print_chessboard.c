320bf9a96dd4af2734a89c8ea29dbb29743ba415
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-04 17:10:02 +0000

    prints the chessboard

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_chessboard - a function that prints the chessboard
+* @a: array of elements
+**/
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%4d", a[i][j]);
+		}
+
+		printf("\n");
+	}
+}
+
