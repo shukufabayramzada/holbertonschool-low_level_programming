94a0ff09ddf606601bfcac1b77d4fa3112b50dac
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-04 17:54:01 +0000

    print diagonal sums

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -9,15 +9,21 @@
 void print_diagsums(int *a, int size)
 {
 	int i;
-	int sum = 0;
+	int sum1 = 0;
 	int sum2 = 0;
 
 	for (i = 0; i < size ; i++)
 	{
-		sum += a[i * size + i];
-	sum2 = a[i * size + (size - i - 1)];
+		sum1 += a[i];
+		a += size;
 	}
-	printf("%d, %d\n", sum, sum2);
+	a -= size;
+	for (i = 0; i < size ; i++)
+	{
+		sum2 -= a[i];
+	a -= size;
+	}
+	printf("%d, %d\n", sum1, sum2);
 }
 
 
