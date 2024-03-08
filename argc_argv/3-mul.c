5009bc12bad584aab2bf7817731291bd69715285
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-08 12:19:19 +0000

    fixed

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,12 +11,17 @@ int main(int argc, char *argv[])
 	int i;
 	int mul = 1;
 
-	if (argc >= 1)
+	if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
 		{
 			mul *= atoi(argv[i]);
 		}
+		printf("%d\n", mul);
+	}
+	else if (argc == 2)
+	{
+		mul = atoi(argv[1]);
 		printf("%d\n", mul);
 	}
 	else
