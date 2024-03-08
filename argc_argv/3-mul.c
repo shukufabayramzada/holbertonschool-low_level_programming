94b9959ae5e3f8d7777b4830cca0e3cf450cbcac
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-08 12:03:21 +0000

    multiply argument vectors except first argument

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function or entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 success.
+ **/
+int main(int argc, char *argv[])
+{
+	int i;
+	int mul = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul *= atoi(argv[i]);
+		}
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
