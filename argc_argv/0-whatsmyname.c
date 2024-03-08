21d2da31383a3b24f43c5d7c8c3abb8dead424c8
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-08 11:19:30 +0000

    print argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - function or entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 success.
+ **/
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
