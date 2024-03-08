ddabeff68e681a60189539d23a8b09ecfb665e03
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-08 11:26:03 +0000

    a program that prints the number of arguments

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - function or entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 success.
+ **/
+int main(int argc, char __attribute__ ((unused)) *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%d\n", argc);
+	}
+	return (0);
+}
