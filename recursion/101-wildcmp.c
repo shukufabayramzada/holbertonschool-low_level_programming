22dafdecb1bd8d8e8f9aea72ae206ec67ecc497b
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-10 17:54:35 +0000

    compare strings in case of wildcards

---
+++ recursion/101-wildcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * wildcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: 1 or 0 depends on the case
+ **/
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
+
