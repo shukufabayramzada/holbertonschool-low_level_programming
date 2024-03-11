4289d20e73aee3b57f51474f2a1c1f97d72dcc8f
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-11 16:27:02 +0000

    concatenates two strings

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,7 +11,15 @@ char *str_concat(char *s1, char *s2)
 {
 	int i, j, len1, len2;
 	char *new_string;
-	
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	for (len1 = 0; s1[len1] != '\0'; len1++)
 	{
 		;
