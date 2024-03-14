6829325c0c15f89ef8fdf57c386562cd2c33f5a2
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-14 17:05:38 +0000

    header file was created

---
+++ structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * dog- user-defined data type
+ * @name: 1st element
+ * @age: 2nd element
+ * @owner: 3rd element
+ **/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
