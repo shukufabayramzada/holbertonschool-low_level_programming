78357f1c55dd9281960a9b7458a12fb82505fada
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-14 18:00:38 +0000

    prints struct dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - initialize the variable of type struct dog
+ * @d: a pointer to the dog
+ **/
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if ((*d).name != NULL)
+		{
+			printf("Name: %s\n", (*d).name);
+		}
+		else
+		{
+			printf("Name: (nil)");
+		}
+		if ((*d).age >= 0)
+		{
+			printf("Age: %.6f\n", (*d).age);
+		}
+		if ((*d).owner != NULL)
+		{
+			printf("Owner: %s\n", (*d).owner);
+		}
+		else
+		{
+			printf("Owner: (nil)");
+		}
+	}
+}
+
