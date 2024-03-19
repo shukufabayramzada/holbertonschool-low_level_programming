a11e2e084a258f6424b03cb2aa9659b6036d10b5
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-19 17:49:42 +0000

    function prints the name through pointer function

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,1 +1,12 @@
 #include "function_pointers.h"
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * print_name - function that prints the name
+ * @name: name that will be printed
+ * @f: pointer function
+ **/
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
