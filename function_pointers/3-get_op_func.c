a2819e959a3ddf7145daad0588206a04b008ab8d
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-20 09:38:11 +0000

    hope it works

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -20,9 +20,14 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
+
+	while (ops[i].op != NULL)
 	{
+		if (*(ops[i].op) == *s && *(s + 1) == '\0')
+		{
+			return (ops[i].f);
+		}
 		i++;
 	}
-	return (ops[i].f);
+	return (NULL);
 }
