66b83fda2db3e8dbaf6540bfcab0840773dfdbb0
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-20 09:55:07 +0000

    main.c updated for betty coding style

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -11,17 +11,16 @@ int main(int argc, char *argv[])
 {
 	int num1, num2;
 	char *op;
-	
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	
 	num1 = atoi(argv[1]);
 	op = argv[2];
 	num2 = atoi(argv[3]);
-	
+
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
@@ -32,7 +31,6 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(100);
 	}
-	
 	printf("%d\n", get_op_func(op)(num1, num2));
 	return (0);
 }
