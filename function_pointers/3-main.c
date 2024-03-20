e3a60157acf16f3f4be21bec7947961bd9b6671b
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-20 09:46:50 +0000

    function that doing different oprations

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 	op = argv[2];
 	num2 = atoi(argv[3]);
 	
-	if (get_op_func(op) == NULL || op[1] != '\0')
+	if (get_op_func(op) == NULL)
 	{
 		printf("Error\n");
 		exit(99);
