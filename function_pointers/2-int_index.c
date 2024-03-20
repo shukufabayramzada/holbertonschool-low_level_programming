6e634df243f27038cadad57a1fb5c1992b921984
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-20 07:49:10 +0000

    function searhes for an integer in the array

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * int_index - function that seraches for an integer
+ * @array: array that we have
+ * @size: size of an array
+ * @cmp: pointer to the function to be used to compare values
+ **/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if ((*cmp)(array[i]))
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
