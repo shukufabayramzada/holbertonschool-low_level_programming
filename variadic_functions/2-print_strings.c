45308a80353f1c73108693a0860b508ab0922788
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-21 09:30:11 +0000

    function that prints the strings that are passed to it

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		str = va_arg(ap, const char *);
 		if (str == NULL)
 		{
-			printf("nil");
+			printf("(nil)");
 		}
 		else
 		{
