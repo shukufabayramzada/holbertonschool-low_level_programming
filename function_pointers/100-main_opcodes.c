0669d33d104405a21c6ca24339eed99fb1948502
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-24 17:53:36 +0000

    read opcodes

--- function_pointers/100-main_opcodes.c
+++ function_pointers/100-main_opcodes.c
@@ -1,32 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_opcodes(int num_bytes) {
-    unsigned char *main_ptr = (unsigned char *)print_opcodes;
-    int i;
-    
-    for (i = 0; i < num_bytes; i++) {
-        printf("%02x", main_ptr[i]);
-        if (i < num_bytes - 1)
-            printf(" ");
-    }
-    printf("\n");
-}
+int main(int argc, char *argv[])
+{
+	int bytes, i;
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Error\n");
-        return 1;
-    }
+	if (argc != 2)
+	{
+	       	printf("Error\n");
+		return (1);
+	}
+	bytes = atoi(argv[1]);
+	
+	if (bytes <= 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	unsigned char *main_ptr = (unsigned char *)main;
 
-    int num_bytes = atoi(argv[1]);
-
-    if (num_bytes <= 0) {
-        printf("Error\n");
-        return 2;
-    }
-
-    print_opcodes(num_bytes);
-    return 0;
+	for (i = 0; i < bytes; i++)
+	{
+		printf("%02hhx ", *(main_ptr + i));
+	}
+	printf("\n");
+	return (0);
 }
 
