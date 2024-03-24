53cada3b7329b903bbfe8d6d278d1d7d93e666b4
shukufabayramzada <shukufabayramzada@gmail.com>
2024-03-24 17:35:52 +0000

    read opcodes

--- function_pointers/100-main_opcodes.c
+++ function_pointers/100-main_opcodes.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[]) {
 
     int num_bytes = atoi(argv[1]);
 
-    if (num_bytes < 0) {
+    if (num_bytes <= 0) {
         printf("Error\n");
         return 2;
     }
