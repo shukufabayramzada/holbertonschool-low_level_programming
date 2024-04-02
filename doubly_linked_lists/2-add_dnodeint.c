a191f4512f4f4cb06a742af9caf232ccd99470f1
shukufabayramzada <shukufabayramzada@gmail.com>
2024-04-02 12:50:27 +0000

    solved compilation error

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -23,7 +23,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	(*new_node).prev = NULL;
 	if (*head != NULL)
 	{
-		(*head).prev = new_node;
+		(*head)->prev = new_node;
 	}
 	*head = new_node;
 	return (new_node);
