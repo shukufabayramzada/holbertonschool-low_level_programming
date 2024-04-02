a1081903ddca8751c49bff483f4e7eaf34d472bd
shukufabayramzada <shukufabayramzada@gmail.com>
2024-04-02 11:33:43 +0000

    updtae style for function

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -14,7 +14,7 @@ size_t print_dlistint(const dlistint_t *h)
 	while (current != NULL)
 	{
 		printf("%d\n", (*current).n);
-		current =(*current).next;
+		current = (*current).next;
 		count++;
 	}
 	return (count);
