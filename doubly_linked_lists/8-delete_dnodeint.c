1ace1bb39a69c9b15a0541c7f0509211d0aa5102
shukufabayramzada <shukufabayramzada@gmail.com>
2024-04-04 19:03:18 +0000

    delete nodes at given index

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -35,7 +35,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if ((*current).next)
 		current->next->prev = current->prev;
 	free (current);
-	return (-1);
+	return (1);
 }
 
 
