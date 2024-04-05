93fd94102fcd5d0ccc618915c806debfa4e10d62
shukufabayramzada <shukufabayramzada@gmail.com>
2024-04-05 18:00:53 +0000

    set 1 at a given index

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * set_bit - set bit at a given index
+ * @n: original number
+ * @index: index
+ * Return: 1 or -1  depends on the case
+ **/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int x = 1;
+
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	x = x << index;
+	*n |= x;
+	return (1);
+}
+
