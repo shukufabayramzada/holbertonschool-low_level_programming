783612c430abe95bb9b3b0873c59fa6daac8cb1e
shukufabayramzada <shukufabayramzada@gmail.com>
2024-04-05 18:22:45 +0000

    count flip bits

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * flip_bits - count the flip bits
+ * @n: number that we have
+ * @m: number to be compared with our number
+ * Return: count
+ **/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int diff = n ^ m;
+	unsigned int count = 0;
+
+	while (diff != 0)
+	{
+		count += diff & 1;
+		diff = diff >> 1;
+	}
+	return (count);
+}
