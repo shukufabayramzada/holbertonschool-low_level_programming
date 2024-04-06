2404f0a0188022e3a7fbf416abc32abf83cfa765
shukufabayramzada <shukufabayramzada@gmail.com>
2024-04-06 18:21:15 +0000

    function that creates a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -6,19 +6,21 @@
 #include <string.h>
 
 /**
- *
+ * create_file - function that creates file
+ * @filename: name of the file
+ * @text_content: buffer
+ * Return: 1 or -1 depends on the case
  **/
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	int len;
-
+	ssize_t bytes_written;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	len = strlen(text_content);
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 	{
@@ -26,13 +28,15 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
-		if (write(fd, text_content, len) == -1)
+		len = strlen(text_content);
+		bytes_written = write(fd, text_content, len);
+		if (bytes_written == -1)
 		{
 			return (-1);
 		}
 	}
 	close(fd);
-	return(1);
+	return (1);
 }
 
 
