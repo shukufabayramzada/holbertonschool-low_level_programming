9441a69074213e658c639398bea4df3abe953fd9
shukufabayramzada <shukufabayramzada@gmail.com>
2024-04-06 17:33:19 +0000

    function that reads a text file and prints it to the POSIX SO

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <fcntl.h>
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: file name
+ * @letters: is the number of letters it should read and print
+ * Return: the actual number of letters it could read and print
+ **/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *m = malloc(letters);
+	int file_des;
+	int count;
+
+	if (filename == NULL || m == NULL)
+	{
+		return (0);
+	}
+	file_des = open(filename, O_RDONLY);
+	count = read(file_des, m, letters);
+	if (count == -1)
+	{
+		return (0);
+	}
+	if (write(file_des, m, count) == -1)
+	{
+		return (0);
+	}
+	close(file_des);
+	return (count);
+}
