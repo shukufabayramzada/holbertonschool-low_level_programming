fdf99de5ca0b84f7bcdf3929967de7b9b79b1903
shukufabayramzada <shukufabayramzada@gmail.com>
2024-04-06 18:12:57 +0000

    function that creates a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	len = strlen(text_content);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 	{
 		return (-1);
