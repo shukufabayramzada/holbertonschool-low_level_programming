f0973873b51ec2db44046d4d2b866a3fcf3cc77c
shukufabayramzada <shukufabayramzada@gmail.com>
2024-04-06 18:09:58 +0000

    function that creates a file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ *
+ **/
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes_written;
+	int len;
+
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	len = strlen(text_content);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, len);
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return(1);
+}
+
+
+
