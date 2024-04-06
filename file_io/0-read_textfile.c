af69728762e33231484741e23c14a5de2bef7953
shukufabayramzada <shukufabayramzada@gmail.com>
2024-04-06 17:34:40 +0000

    function that reads a text file and prints it to the POSIX SO

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -25,7 +25,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-	if (write(file_des, m, count) == -1)
+	if (write(STDOUT_FILENO, m, count) == -1)
 	{
 		return (0);
 	}
