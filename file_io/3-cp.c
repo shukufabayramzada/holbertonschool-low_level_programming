88ce6192cf465f6434a6863d3c9d548826d56794
shukufabayramzada <shukufabayramzada@gmail.com>
2024-04-07 10:06:41 +0000

    function that copies the text

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
 	}
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
