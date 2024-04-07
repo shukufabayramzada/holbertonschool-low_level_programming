e32ad7c1ca6287323826a0b4bef12a4e709c6d1d
shukufabayramzada <shukufabayramzada@gmail.com>
2024-04-07 10:08:31 +0000

    function that copies the text

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from the file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
@@ -45,7 +45,7 @@ int main(int argc, char *argv[])
 	}
 	if (bytes_read == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from the file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	if (close(fd_from) == -1)
